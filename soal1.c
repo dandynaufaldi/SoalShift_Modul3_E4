37d04f584013f30dfb93c5d59c9e42753d1db949
nawamkihafahd <nawamkihafahd@gmail.com>
2017-10-25 00:12:30 +0700

    soal1 -> rough layout : finishing, not tested

--- soal1.c
+++ soal1.c
@@ -28,37 +28,105 @@ void penjual()
     }
     while(1)
     {
-	printf("Pilih Aksi:\n1. Tambah stock senjata\n2. Lihat stock senjata");
+	printf("Pilih Aksi:\n1. tambah stock senjata\n2. lihat stock senjata");
 	scanf("%d",&modej);
 	if(modej==1)
 	{
 	    scanf("%s %d",senjataj, amountj);
-	    if(strcmp(senjataj, senjata[0])
+	    if(!strcmp(senjataj, senjata[0]))
 	    {
 		*weapon[0] += amountj;
 	    }
-	    else if(strcmp(senjataj, senjata[1])
+	    else if(!strcmp(senjataj, senjata[1]))
 	    {
 		*weapon[1] += amountj;
 	    }
-	    else if(strcmp(senjataj, senjata[2])
+	    else if(!strcmp(senjataj, senjata[2]))
 	    {
 		*weapon[2] += amountj;
 	    }
-	    else if(strcmp(senjataj, senjata[3])
+	    else if(!strcmp(senjataj, senjata[3]))
 	    {
 		*weapon[3] += amountj;
 	    }
-	    else if(strcmp(senjataj, senjata[4])
+	    else if(!strcmp(senjataj, senjata[4]))
 	    {
 		*weapon[4] += amountj;
 	    }
-	    else if(strcmp(senjataj, senjata[5])
+	    else if(!strcmp(senjataj, senjata[5]))
 	    {
 		*weapon[5] += amountj;
 	    }
 	}
 	else if(modej==2)
+	{
+	    for(int i=0;i<6;i++)
+	    {
+		printf("%s %lld",senjata[i], *weapon[i]);
+	    }
+	}
+	else
+	{
+	    printf("Silahkan Coba Lagi\n);
+	}
+    }
+}
+void pembeli()
+{
+    int modeb;
+    int amountb;
+    char senjatab[8];
+    while(1)
+    {
+	printf("Pilih Aksi:\n1. beli senjata\n2. lihat stock senjata");
+	scanf("%d",&modeb);
+	if(modeb==1)
+	{
+	    scanf("%s %d",senjatab, amountb);
+	    if(!strcmp(senjatab, senjata[0]))
+	    {
+		if(amountb <= *weapon[0])
+		*weapon[0] -= amountb;
+		else
+		printf("barang di stock tidak cukup\n");
+	    }
+	    else if(!strcmp(senjatab, senjata[1]))
+	    {
+		if(amountb <= *weapon[1])
+		*weapon[1] -= amountb;
+		else
+		printf("barang di stock tidak cukup\n");
+	    }
+	    else if(!strcmp(senjatab, senjata[2]))
+	    {
+		if(amountb <= *weapon[2])
+		*weapon[2] -= amountb;
+		else
+		printf("barang di stock tidak cukup\n");
+	    }
+	    else if(!strcmp(senjatab, senjata[3]))
+	    {
+		if(amountb <= *weapon[3])
+		*weapon[3] -= amountb;
+		else
+		printf("barang di stock tidak cukup\n");
+	    }
+	    else if(!strcmp(senjatab, senjata[4]))
+	    {
+		if(amountb <= *weapon[4])
+		*weapon[4] -= amountb;
+		else
+		printf("barang di stock tidak cukup\n");
+	    }
+	    else if(!strcmp(senjatab, senjata[5]))
+	    {
+		if(amountb <= *weapon[5])
+		*weapon[5] -= amountb;
+		else
+		printf("barang di stock tidak cukup\n");
+	    }
+	}
+	else if(modeb==2)
 	{
 	    for(int i=0;i<6;i++)
 	    {
@@ -72,13 +140,6 @@ void penjual()
 	{
 	    printf("Silahkan Coba Lagi\n);
 	}
-    }
-}
-void pembeli()
-{
-    int modeb;
-    while(1)
-    {
     }
 }
 
